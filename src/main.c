968523323c015453cbabca0b8bd90a8608268a4a
Mary Ivanova <78850640+MaryIv76@users.noreply.github.com>
2022-03-12 08:05:06 +0000

    Add reading data from the command line and output to the screen

--- src/main.c
+++ src/main.c
@@ -1,8 +1,20 @@
-/* hello.c */
+/* Author: Mariya Ivanova */
+/* main.c */
+/* Сalculation of the total number of a given letter in three
+given sentences */
 #include <stdio.h>
 
-void main (void)
+int main (int argc, char *argv[])
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  if (argc != 5)
+  {
+    printf("The number of arguments must be 5");
+    return 1;
+  }
+
+  char* letter = argv[1];
+  printf("Letter to count: %s\n", letter);
+  printf("Given sentences:\n\t%s\n\t%s\n\t%s\n", argv[2], argv[3], argv[4]);
+  
+	return 0;
 }
