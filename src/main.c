570492cca2c762f733c6ebc43016d1c0985b7fce
Mary Ivanova <78850640+MaryIv76@users.noreply.github.com>
2022-03-12 09:32:18 +0000

    Add function countLetterInSentence

--- src/main.c
+++ src/main.c
@@ -3,6 +3,23 @@
 /* Сalculation of the total number of a given letter in three
 given sentences */
 #include <stdio.h>
+#include <string.h>
+
+int countLetterInSentence(char* letter, char* sentence)
+{
+  int counter = 0;
+  int length = strlen(sentence);
+
+  for (int i = 0; i < length; i++)
+  {
+    if (*letter == sentence[i])
+    {
+      counter++;
+    }
+  }
+
+  return counter;
+}
 
 int main (int argc, char *argv[])
 {
@@ -15,6 +32,13 @@ int main (int argc, char *argv[])
   char* letter = argv[1];
   printf("Letter to count: %s\n", letter);
   printf("Given sentences:\n\t%s\n\t%s\n\t%s\n", argv[2], argv[3], argv[4]);
+
+  int counter = 0;
+  counter += countLetterInSentence(letter, argv[2]);
+  counter += countLetterInSentence(letter, argv[3]);
+  counter += countLetterInSentence(letter, argv[4]);
+
+  printf("\nLetter %s: %d time(s)\n", letter, counter);
   
 	return 0;
 }
